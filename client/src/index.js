bac66913d445be7970f7b8306eea7ed1c426b07e
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-09-11 19:54:18 +0200

    chore(bootstrap): clean React imports and wrap App with Router/Provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,18 @@
-import React from from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
