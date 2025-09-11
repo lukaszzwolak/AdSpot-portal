9304e15f36ef0c8574b92b5a0a88378b9cd6dc94
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-09-11 19:38:47 +0200

    fix(client): users redux slice and initial state naming

---
+++ client/src/redux/store.js
@@ -0,0 +1,24 @@
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import initialState from "./initialState";
+import adsReducer from "./adsRedux";
+import usersReducer from "./usersRedux";
+
+const subreducers = {
+  ads: adsReducer,
+  users: usersReducer,
+};
+
+const reducer = combineReducers(subreducers);
+const store = createStore(
+  reducer,
+  initialState,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f
+  )
+);
+
+export default store;
