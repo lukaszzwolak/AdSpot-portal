13ccbe874ee177dccc9a26eb7822ea9a6c170d72
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-09-13 13:13:48 +0200

    chore(redux): migrate store to Redux Toolkit and add ads placeholder reducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,24 +1,13 @@
-import { createStore, combineReducers, compose, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import initialState from "./initialState";
+import { configureStore } from "@reduxjs/toolkit";
 import adsReducer from "./adsRedux";
 import usersReducer from "./usersRedux";
 
-const subreducers = {
-  ads: adsReducer,
-  users: usersReducer,
-};
-
-const reducer = combineReducers(subreducers);
-const store = createStore(
-  reducer,
-  initialState,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__
-      ? window.__REDUX_DEVTOOLS_EXTENSION__()
-      : (f) => f
-  )
-);
+const store = configureStore({
+  reducer: {
+    ads: adsReducer,
+    users: usersReducer,
+  },
+  devTools: true,
+});
 
 export default store;
